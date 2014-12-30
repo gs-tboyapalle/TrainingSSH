6df179040b09124e9e2bab186b45a45bd1b994ec
tboyapalle <tboyapalle@gainsight.com>
2014-12-30 17:53:58 +0530

    Command line args commit

---
+++ src/assign1/Today.java
@@ -0,0 +1,16 @@
+package assign1;
+
+public class Today {
+	public static void main(String args[])
+	{
+		/*System.out.println(" ______");
+		System.out.println(" |     |");
+		//System.out.println("|	|");
+		System.out.print("______"); */
+		for(int i=0;i<3;i++)
+			System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+	}
+
+}
